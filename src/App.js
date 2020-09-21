bdd9c9b4209fe7b8b0059514abc8e71e1a169af7
Elena Dragunova <elena.dragunova@saritasa.com>
2020-09-21 10:45:05 +0500

    Add Accordion component.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,25 @@
 import React from 'react'
+import Accordion from './components/Accordion'
+
+const items = [
+  {
+    title: 'What is React',
+    content: 'React is a JS framework',
+  },
+  {
+    title: 'Why use React',
+    content: 'React is a favorite JS framework among many engineers',
+  },
+  {
+    title: 'How dow you use React',
+    content: 'You use React by creating components',
+  },
+];
 
 export default () => {
-  return <h1>Widgets App</h1>
+  return (
+    <div>
+      <Accordion items={items}/>
+    </div>
+  )
 }
