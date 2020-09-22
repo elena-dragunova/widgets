98f101283b036216a37d268fdeffaeff7a4bb8da
elena-dragunova <elenak-8647@gmail.com>
2020-09-22 09:48:27 +0500

    Add custom navigation.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import React, { useState } from 'react'
 import Accordion from './components/Accordion'
 import Search from './components/Search'
 import Translate from './components/Translate'
+import Dropdown from './components/Dropdown';
+import Route from './components/Route';
+import Header from './components/Header';
 
 const items = [
   {
@@ -33,12 +36,28 @@ const options = [
   },
 ];
 
+
 export default () => {
-  // const [selected, setSelected] = useState(options[0]);
+  const [selected, setSelected] = useState(options[0]);
 
   return (
     <div>
-      <Translate />
+      <Header />
+      <Route path="/">
+        <Accordion items={items} />
+      </Route>
+      <Route path="/list">
+        <Search />
+      </Route>
+      <Route path="/dropdown">
+        <Dropdown label="Select a Color"
+                  options={options}
+                  selected={selected}
+                  onSelectedChange={setSelected}/>
+      </Route>
+      <Route path="/translate">
+        <Translate />
+      </Route>
     </div>
   )
 }
